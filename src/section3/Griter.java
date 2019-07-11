b2295881074760a56599153dc65f29be53148c50
League Student <league@iMac-8.attlocal.net>
2019-07-11 14:32:36 -0700

    day4

--- src/section3/Griter.java
+++ src/section3/Griter.java
@@ -5,6 +5,6 @@ import javax.swing.JOptionPane;
 public class Griter {
  public static void main(String[] args) {
 	String answer=JOptionPane.showInputDialog("what's ur name?");
-	JOptionPane.showMessageDialog(null,"Hello"+answer);
+	JOptionPane.showMessageDialog(null,"Hello "+answer);
 }
 }
